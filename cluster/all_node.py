a8519899b191dc37899e0451a3ff2ccbafa0ff92
Sanket Patel <sanket.devops@gmail.com>
2023-01-31 14:58:45 -0500

    Sanket: add hosname, hosts, and swap function

--- cluster/all_node.py
+++ cluster/all_node.py
@@ -4,9 +4,47 @@ from cluster.modules.ssh import ssh_conn
 
 def setup_all_node(servers):
     for server in servers:
-        print(server["host"])
+        id = server["id"]
+        host = server["host"]
+        username = server["username"]
+        password = server["password"]
+        hostname = server["hostname"]
+        role = server["role"]
+        master = server["master"]
+        
+        def set_hostname(server):
+            print("====================>[Set Hostname]=>[ {} = {} ]<====================".format(hostname, host))
+            commandsArr = ["hostnamectl set-hostname {}".format(hostname),"cat /etc/hostname"]
+            res = ssh_conn(host, username, password, commandsArr)
+            print(res)
+        # set_hostname(server)
 
+        def set_hosts(servers):
+            print("====================>[Set Hosts]=>[ {} = {} ]<====================".format(hostname, host))
+            for node in servers:
+                def set_hostEntry():
+                    commandsArr = ["cat >>/etc/hosts<<EOF\n{}    {}\nEOF".format(node["host"], node["hostname"]),"cat /etc/hosts"]
+                    res = ssh_conn(host, username, password, commandsArr)
+                    # print(res)
+                    return res
+                commands_check = ["cat /etc/hosts | grep '{}    {}'".format(node["host"], node["hostname"])]
+                res = ssh_conn(host, username, password, commands_check)
+                # print(len(res))
+                for resData in res:
+                    # print(len(data))
+                    if len(resData) == 0:
+                        set_hostEntry()
+                        print("Host entery set...")
+                    else:
+                        print("Host entery already set...")
+        # set_hosts(servers)
 
+        def swap_off(server):
+            print("====================>[Set Hostname]=>[ {} = {} ]<====================".format(hostname, host))
+            commandsArr = ["sed -i '/swap/d' /etc/fstab", "swapoff -a"]
+            res = ssh_conn(host, username, password, commandsArr)
+            print(res)
+        swap_off(server)
 
 
     
--- cluster/modules/ssh.py
+++ cluster/modules/ssh.py
@@ -1,13 +1,14 @@
 import paramiko
 import sys
+import os
 
-async def ssh_conn(host, username, password, commandsArr):
+def ssh_conn(host, username, password, commandsArr):
     hostName = host
     hostUser = username
     hostPass = password
     commands = commandsArr
     results = []
-
+    os.system("ssh-keygen -R {} > /dev/null 2>&1".format(hostName))
     client = paramiko.SSHClient()
     client.load_system_host_keys()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -19,5 +20,5 @@ async def ssh_conn(host, username, password, commandsArr):
             nRemove = line.strip('\n')
             temp.append(nRemove)
         results.append(temp)
-    print("Host: ", hostName,"\nData: ", results)
+    # print("Host: ", hostName,"\nData: ", results)
     return results
