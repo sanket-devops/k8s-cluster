11de57defb1e5bc7f8e0d159c67948193f6e9d36
Sanket Patel <sanket.devops@gmail.com>
2025-06-06 19:00:35 -0400

    Sanket: Fix ssh access and increase vm memory

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -2,15 +2,21 @@
 
 # Enable ssh password authentication
 echo "Enable ssh password authentication"
-sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+sed -i 's/.*PasswordAuthentication.*/PasswordAuthentication yes/' /etc/ssh/sshd_config
 sed -i 's/.*PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
 sed -i 's/.*PubkeyAuthentication.*/PubkeyAuthentication yes/' /etc/ssh/sshd_config
 # Enable powershell remote session
 sed -i '$ a Subsystem powershell /usr/bin/pwsh -sshs -NoLogo' /etc/ssh/sshd_config
 
+# Remove another sshd_config
+rm -rf /etc/ssh/sshd_config.d/*
+
 # Restart sshd
 systemctl reload sshd
 # systemctl restart sshd
+
+# Verify SSH Config
+sudo sshd -T | grep -Ei 'passwordauthentication|pubkeyauthentication|permitrootlogin'
 
 # Set Root password
 echo "Set root password"
