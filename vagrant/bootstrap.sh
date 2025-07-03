1791b546e07d6158b1ec8bd8d54a9bc08ae81248
Sanket Patel <sanket.devops@gmail.com>
2025-07-03 14:11:28 -0400

    Sanket: Enhance code

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -21,25 +21,3 @@ sudo sshd -T | grep -Ei 'passwordauthentication|pubkeyauthentication|permitrootl
 # Set Root password
 echo "Set root password"
 echo -e "admin\nadmin" | passwd root >/dev/null 2>&1
-
-
-
-# Install PowerShell
-
-# Update the list of packages
-apt-get update
-
-# Install pre-requisite packages.
-apt-get install -y wget apt-transport-https software-properties-common
-
-# Download the Microsoft repository GPG keys
-wget -q "https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/packages-microsoft-prod.deb"
-
-# Register the Microsoft repository GPG keys
-dpkg -i packages-microsoft-prod.deb
-
-# Update the list of packages after we added packages.microsoft.com
-apt-get update
-
-# Install PowerShell
-apt-get install -y powershell
